fc8582ee5d00ee6c351812aa20de2dac9dba637e
JhonnyHerbas <jerbasjhon@gmail.com>
2023-03-31 15:24:45 -0400

    Templates

---
+++ App/views/head.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php echo $title; ?>
+    </title>
+    <link rel="shortcut icon" href="/SistemaParqueo/public/img/FCYT.png"/>
+    <link rel="stylesheet" type="text/css" href="/SistemaParqueo/public/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/SistemaParqueo/public/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+</head>
---
+++ App/views/scripts.php
@@ -0,0 +1,2 @@
+<script src="/SistemaParqueo/public/js/script.js"></script>
+<script src="/SistemaParqueo/public/js/bootstrap.bundle.js"></script>
