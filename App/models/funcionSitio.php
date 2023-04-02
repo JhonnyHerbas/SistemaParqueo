170b73ebaf330a573b79855826e3222092b04ea5
JhonnyHerbas <jerbasjhon@gmail.com>
2023-04-02 12:02:37 -0400

    Paso de parametros a editar

--- App/models/funcionSitio.php
+++ App/models/funcionSitio.php
@@ -4,12 +4,20 @@ require_once ('../config/conexion.php');
 
 function visualizar_sitio($cod) {
     $conn = get_connection();
-    //$query = 'CALL DB_SP_SITIO_VISTA_ID_SEC("'.$cod.'")';
+    $result = mysqli_query($conn, "CALL DB_SP_SITIO_VISTA_ID_SEC($cod)");
 
-    $query = "Select * from SITIO where ID_SEC = $cod";
+    if (mysqli_num_rows($result) > 0) {
+        return $result;
+    } else {
+        return null;
+    }
+}
 
-    $result = $conn->query($query);
-    if ($result = $conn->query($query)) {
+function buscar_sitio($nombre) {
+    $conn = get_connection();
+    $result = mysqli_query($conn, "CALL DB_SP_SITIO_BUSCAR($nombre)");
+
+    if (mysqli_num_rows($result) > 0) {
         return $result;
     } else {
         return null;
--- App/views/visualizarSitio.php
+++ App/views/visualizarSitio.php
@@ -5,16 +5,16 @@
 
 $title = "Buscar sitio";
 include('head.php');
-    
+
 ?>
 
 <body>
     <!-- Include del header.php -->
-    <?php 
-    
+    <?php
+
     $user = "Jhonny Herbas";
     $role = "Administrador";
-    $lista =    "<ul>
+    $lista = "<ul>
                     <li><a href=''>Inicio</a></li>
                     <li><a href=''>Visualizar</a></li>
                     <li><a href=''>Configurar horario</a></li>
@@ -32,7 +32,7 @@ include('head.php');
     <main>
         <div class="main">
             <div class="container-busqueda">
-                <form action="" method="">
+                <form action="" method="POST">
                     <div class="input-container">
                         <div class="container-input-buscar">
                             <input type="text" class="input-buscar">
@@ -42,98 +42,98 @@ include('head.php');
                         </div>
                     </div>
                 </form>
-            </div> 
+            </div>
             <div class="visual">
                 <div class="container-seccion">
                     <select class="seccion">
                         <?php
                         
-                        $i= 1;  
                         $result = visualizar_seccion();
-                        if($result){
-                            while($row = $result->fetch_array(MYSQLI_BOTH)){
+                        if ($result) {
+                            while ($row = $result->fetch_array(MYSQLI_BOTH)) {
                                 $id = $row['ID_SEC'];
                                 $nombre = $row['NOMBRE_SEC'];
                                 echo "<option value='$id'>$nombre</option>";
                             }
-                        } 
+                        }
                         ?>
                     </select>
                 </div>
                 <div class="container-vista">
-                    <div class="accordion" id="accordionPanelsStayOpenExample">
-                        <?php 
-                        
-                        $i = 1;
-                        $result = visualizar_sitio(1);
-                        if ($result) {
-                            while ($row= $result->fetch_array(MYSQLI_BOTH)){
-                                  
-                        ?>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                                <?php 
-                                
-                                echo "Sitio #".$row["NOMBRE_SIT"];
-                                $i = $i+1;
-                                
+                        <?php
+
+                        $sitios = visualizar_sitio(1);
+                        if ($sitios) {
+                            while ($fila = mysqli_fetch_array($sitios)) {
+                                $buttons = "flush-collapse";
                                 ?>
-                            </button>
-                            </h2>
-                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                            <div class="accordion-body body-sitio">
-                                <div class="acordion-text w-50">
-                                    <?php 
-                                    
-                                    if ($row["DISPONIBLE_SIT"] == 1) {
-                                        echo "Disponible: Si<br>";
-                                    } else {
-                                        echo "Disponible: No<br>";
-                                    }
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#<?php echo $buttons.$fila["ID_SIT"];?>" aria-expanded="false"
+                                            aria-controls="flush-collapseOne">
+                                            <?php
 
-                                    if ($row["DISPONIBLE_SIT"] == 1) {
-                                        echo "Asignado: No<br>";
-                                    } else {
-                                        echo "Asignado: Si<br>";
-                                    }
+                                            echo "Sitio #" . $fila["NOMBRE_SIT"];
 
-                                    echo "Precio: ".$row["PRECIO_SIT"]." PARCK";
+                                            ?>
+                                        </button>
+                                    </h2>
+                                    <div id="<?php echo $buttons.$fila["ID_SIT"];?>" class="accordion-collapse collapse"
+                                        aria-labelledby="<?php echo $buttons.$fila["ID_SIT"];?>" data-bs-parent="#accordionFlushExample">
+                                        <div class="accordion-body body-sitio">
+                                            <div class="acordion-text w-50">
+                                                <?php
 
-                                    ?>
+                                                if ($fila["DISPONIBLE_SIT"] == 1) {
+                                                    echo "Disponible: Si<br>";
+                                                } else {
+                                                    echo "Disponible: No<br>";
+                                                }
+
+                                                if ($fila["DISPONIBLE_SIT"] == 1) {
+                                                    echo "Asignado: No<br>";
+                                                } else {
+                                                    echo "Asignado: Si<br>";
+                                                }
+
+                                                echo "Precio: " . $fila["PRECIO_SIT"] . " PARCK";
+
+                                                ?>
+                                            </div>
+                                            <div class="acordion-btn w-50">
+                                                <div class="function celeste">
+                                                    <a href="" target="_self" class="fa-solid fa-lock blanco"></a>
+                                                </div>
+                                                <div class="function verde">
+                                                    <a href="" target="_self" class="fa-solid fa-id-card-clip blanco"></a>
+                                                </div>
+                                                <div class="function azul">
+                                                    <a href="editarSitio.php?nombre=<?php echo $fila['NOMBRE_SIT'];?>&precio=<?php echo $fila['PRECIO_SIT'];?>" target="_self" class="fa-solid fa-pencil blanco"></a>
+                                                </div>
+                                                <div class="function rojo">
+                                                    <a href="" target="_self" class="fa-solid fa-trash blanco"></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="acordion-btn w-50">
-                                    <div class="function celeste">
-                                        <a href="" target="_blank" class="fa-solid fa-lock blanco"></a>
-                                    </div>
-                                    <div class="function verde">
-                                        <a href="" target="_blank" class="fa-solid fa-id-card-clip blanco"></a>
-                                    </div>
-                                    <div class="function azul">
-                                        <a href="" target="_blank" class="fa-solid fa-pencil blanco"></a>
-                                    </div>
-                                    <div class="function rojo">
-                                        <a href="" target="_blank" class="fa-solid fa-trash blanco"></a>
-                                    </div>  
-                                </div>
-                            </div>
-                        </div>
-                        <?php 
+                                <?php
                             }
                         }
                         ?>
-                    </div>
                 </div>
             </div>
-        </div>                   
+        </div>
     </main>
 
     <!-- Include de los scripts.php -->
     <?php
-    
+
     include('scripts.php');
 
     ?>
 
 </body>
+
 </html>
