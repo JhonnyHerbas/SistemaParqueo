91b1faa8478b47e3e2322ee6c33434e360cb2640
JhonnyHerbas <jerbasjhon@gmail.com>
2023-04-14 22:15:13 -0400

    logins y cambios de forms en varios inputs

---
+++ App/views/iniciarSesionAdmin.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+
+$title = "Iniciar sesión";
+include('templates/head.php');
+include('../models/funcionSolicitud.php');
+?>
+
+<body>
+    <?php
+
+    $user = "Jhonny Herbas";
+    $role = "Administrador";
+
+    include('templates/header.php');
+    ?>
+    <main>
+        <?php
+
+        $title = "Iniciar sesión";
+        $action = "";
+        $metodo = "POST";
+        include('templates/iniForm.php');
+        ?>
+
+        <h1>Administrador</h1>
+
+        <!-- Aqui viene toda la interfaz de visualizacion -->
+        <div class="mb-3">
+            <label for="validationCustom01" class="form-label">Nombre de usuario:</label>
+            <input type="text" name="titulo-solicitud" class="form-control" id="validationCustom01" pattern="[a-zA-Z]{5,30}"
+                autocomplete="off" spellcheck="false" placeholder="Ingrese su código" required>
+            <div class="invalid-feedback">
+                Por favor, ingrese un usuario válido.
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="validationCustom02" class="form-label">Contraseña</label>
+            <input type="password" name="titulo-solicitud" class="form-control" id="validationCustom02"
+                pattern="^[a-zA-Z0-9]{8,20}$" autocomplete="off" spellcheck="false" placeholder="Ingrese su contraseña"
+                required>
+            <div class="invalid-feedback">
+                Por favor, ingrese un nombre válido.
+            </div>
+        </div>
+        <div class="button-container">
+            <button class="btn btn-success" id="submitButton" data-toggle="modal" data-target="#exampleModal">Iniciar
+                sesión</button>
+        </div>
+        <div class="d-flex align-items-center justify-content-center h5">
+            <a href="#">¿Olvidaste tu contraseña?</a>
+        </div>
+
+        <?php
+
+        include('templates/finForm.php');
+        ?>
+
+    </main>
+
+
+    <?php
+
+    include('templates/scripts.php');
+
+    ?>
+    <script src="/SistemaParqueo/public/js/validacion.js"></script>
+</body>
+
+</html>
--- App/views/registrarCliente.php
+++ App/views/registrarCliente.php
@@ -19,112 +19,115 @@ include('../models/funcionSolicitud.php');
 
     <!-- Aqui vendra toda la interfaz que se necesita para la visualizacion -->
     <main>
-        <?php
+        <div class="form-containerG">
+            <div class="header-containerG">
+                <h2 class="title-form">
+                    Registrar docente
+                </h2>
+            </div>
+            <div class="card-body grande">
+                <form id="myForm" class="row g-3 needs-validation" novalidate action="<?php echo $action; ?>"
+                    method="post">
 
-        $title = "Registrar docente";
-        $action = "";
-        $metodo = "POST";
-        include('templates/iniForm.php');
-        ?>
+                    <div class="columnas">
+                        <div class="izquierda">
+                            <!-- Input del nombre -->
+                            <div class="mb-3">
+                                <label for="validationCustom01" class="form-label">Nombre:</label>
+                                <input type="text" name="nombre" class="form-control" id="validationCustom01"
+                                    pattern="[a-zA-Z\s]{3,30}" autocomplete="off" spellcheck="false"
+                                    placeholder="Ingrese su nombre" required>
+                                <div class="invalid-feedback">
+                                    Ingrese un nombre válido.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="derecha">
+                            <div class="mb-3">
+                                <label for="validationCustom02" class="form-label">Apellido/s:</label>
+                                <input type="text" name="apellido" class="form-control" id="validationCustom02"
+                                    pattern="[a-zA-Z\s]{3,90}" autocomplete="off" spellcheck="false" minlength="5"
+                                    maxlength="30" placeholder="Ingrese su apellido" required>
+                                <div class="invalid-feedback">
+                                    Ingrese un apellido válido.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columnas">
+                        <div class="izquierda">
+                            <!-- Input del nombre -->
+                            <div class="mb-3">
+                                <label for="validationCustom03" class="form-label">Código SIS:</label>
+                                <input type="text" name="codigo" class="form-control" id="validationCustom03"
+                                    pattern="^[0-9]{9}$" autocomplete="off" spellcheck="false" minlength="5"
+                                    maxlength="30" placeholder="Ingrese su código" required>
+                                <div class="invalid-feedback">
+                                    Ingrese un código válido.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="derecha">
+                            <div class="mb-3">
+                                <label for="validationCustom04" class="form-label">Celular:</label>
+                                <input type="text" name="celular" class="form-control" id="validationCustom04"
+                                    pattern="^[0-9]{8}$" autocomplete="off" spellcheck="false" minlength="5"
+                                    maxlength="30" placeholder="Ingrese su numero" required>
+                                <div class="invalid-feedback">
+                                    Ingrese un numero válido.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columnas">
+                        <div class="izquierda">
+                            <!-- Input del nombre -->
+                            <div class="mb-3">
+                                <label for="validationCustom05" class="form-label">Correo:</label>
+                                <input type="email" name="correo" class="form-control" id="validationCustom05"
+                                    autocomplete="off" spellcheck="false" maxlength="50" placeholder="Ingrese su correo"
+                                    required>
+                                <div class="invalid-feedback">
+                                    Ingrese un correo válido.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="derecha">
+                            <div class="mb-3">
+                                <label for="validationCustom06" class="form-label">Contraseña:</label>
+                                <input type="password" name="pass" class="form-control" id="validationCustom06"
+                                    pattern="^[a-zA-Z0-9]{8,20}$" autocomplete="off" spellcheck="false"
+                                    placeholder="Ingrese su contraseña" required>
+                                <div class="invalid-feedback">
+                                    Ingrese una contraseña válida.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columnas">
+                        <div class="izquierda">
+                            <!-- Input de verificar contraseña -->
+                            <div class="mb-3">
+                                <label for="validationCustom07" class="form-label">Verificar contraseña:</label>
+                                <input type="password" name="verPass" class="form-control" id="validationCustom07"
+                                    pattern="^[a-zA-Z0-9]{8,20}$" autocomplete="off" spellcheck="false"
+                                    placeholder="Ingrese su contraseña" required>
+                                <div class="invalid-feedback">
+                                    Ingrese una contraseña válida.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                    $success = "Guardar";
+                    $danger = "Cancelar";
+                    include('templates/buttonsForms.php');
 
-        <div class="overflow-registro">
-            <div class="columnas">
-                <div class="izquierda">
-                    <!-- Input del nombre -->
-                    <div class="mb-3">
-                        <label for="validationCustom01" class="form-label">Nombre:</label>
-                        <input type="text" name="titulo-solicitud" class="form-control" id="validationCustom01"
-                            pattern="^[a-zA-Z0-9\s]*$" autocomplete="off" spellcheck="false" minlength="5"
-                            maxlength="30" placeholder="Ingrese su nombre" required>
-                        <div class="invalid-feedback">
-                            Por favor, ingrese un nombre válido.
-                        </div>
-                    </div>
-                </div>
-                <div class="derecha">
-                    <div class="mb-3">
-                        <label for="validationCustom02" class="form-label">Apellido/s:</label>
-                        <input type="text" name="titulo-solicitud" class="form-control" id="validationCustom02"
-                            pattern="^[a-zA-Z0-9\s]*$" autocomplete="off" spellcheck="false" minlength="5"
-                            maxlength="30" placeholder="Ingrese su apellido" required>
-                        <div class="invalid-feedback">
-                            Por favor, ingrese un apellido válido.
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="columnas">
-                <div class="izquierda">
-                    <!-- Input del nombre -->
-                    <div class="mb-3">
-                        <label for="validationCustom03" class="form-label">Codigo SIS:</label>
-                        <input type="text" name="titulo-solicitud" class="form-control" id="validationCustom03"
-                            pattern="^[a-zA-Z0-9\s]*$" autocomplete="off" spellcheck="false" minlength="5"
-                            maxlength="30" placeholder="Ingrese su nombre" required>
-                        <div class="invalid-feedback">
-                            Por favor, ingrese un codigo válido.
-                        </div>
-                    </div>
-                </div>
-                <div class="derecha">
-                    <div class="mb-3">
-                        <label for="validationCustom04" class="form-label">Celular:</label>
-                        <input type="text" name="titulo-solicitud" class="form-control" id="validationCustom04"
-                            pattern="^[a-zA-Z0-9\s]*$" autocomplete="off" spellcheck="false" minlength="5"
-                            maxlength="30" placeholder="Ingrese su apellido" required>
-                        <div class="invalid-feedback">
-                            Por favor, ingrese un numero válido.
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="columnas">
-                <div class="izquierda">
-                    <!-- Input del nombre -->
-                    <div class="mb-3">
-                        <label for="validationCustom05" class="form-label">Correo:</label>
-                        <input type="text" name="titulo-solicitud" class="form-control" id="validationCustom05"
-                            pattern="^[a-zA-Z0-9\s]*$" autocomplete="off" spellcheck="false" minlength="5"
-                            maxlength="30" placeholder="Ingrese su nombre" required>
-                        <div class="invalid-feedback">
-                            Por favor, ingrese un correo válido.
-                        </div>
-                    </div>
-                </div>
-                <div class="derecha">
-                    <div class="mb-3">
-                        <label for="validationCustom06" class="form-label">Contraseña:</label>
-                        <input type="password" name="titulo-solicitud" class="form-control" id="validationCustom06"
-                            pattern="^[a-zA-Z0-9\s]*$" autocomplete="off" spellcheck="false" minlength="5"
-                            maxlength="30" placeholder="Ingrese su apellido" required>
-                        <div class="invalid-feedback">
-                            Por favor, ingrese una contraseña válida.
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="columnas">
-                <div class="izquierda">
-                    <!-- Input de verificar contraseña -->
-                    <div class="mb-3">
-                        <label for="validationCustom07" class="form-label">Verificar contraseña:</label>
-                        <input type="password" name="titulo-solicitud" class="form-control" id="validationCustom07"
-                            pattern="^[a-zA-Z0-9\s]*$" autocomplete="off" spellcheck="false" minlength="5"
-                            maxlength="30" placeholder="Ingrese su nombre" required>
-                        <div class="invalid-feedback">
-                            Por favor, ingrese una contraseña válida.
-                        </div>
-                    </div>
-                </div>
+                    include('templates/finForm.php');
+                    ?>
+                </form>
             </div>
         </div>
-        <?php
-        $success = "Guardar";
-        $danger = "Cancelar";
-        include('templates/buttonsForms.php');
-
-        include('templates/finForm.php');
-        ?>
     </main>
 
 
