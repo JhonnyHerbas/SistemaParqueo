773af2d608ce29a1db0462b84219dc475ece9b20
jhamil06 <201801948@est.umss.edu>
2023-04-23 16:36:18 -0400

    arreglos-configuracion

--- App/views/configuracionHorario.php
+++ App/views/configuracionHorario.php
@@ -5,6 +5,7 @@
 
 $title = "Configurar horario";
 include('templates/head.php');
+
 include('../models/funcionConfiguracionHorario.php')
 ?>
 
@@ -25,23 +26,23 @@ include('../models/funcionConfiguracionHorario.php')
             <h2 class="h2">Registrar nuevo horario</h2>
         </div>
         <div class="card-body">
-            <form id="myForm" class="row g-3 needs-validation" novalidate action="/SistemaParqueo/App/controllers/configuracionHorarioAction.php" method="post">
+            <form id="myForm" class="row g-3 needs-validation" novalidate action="/SistemaParqueo/App/controllers/configuracionHorarioAction.php" method="post" novalidate>
                 <div class="mb-3">
                     <label for="validationCustom01" class="form-label">Horario de apertura:</label>
                     <input type="text" name="hora-apertura" class="form-control" id="validationCustom01" pattern="^(0[6-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$" autocomplete="off" spellcheck="false" 
                     minlength="8" maxlength="8" placeholder="HH:MM:SS" required>
-                    <div class="invalid-feedback">
+                    <div class="invalid-feedback" >
                         Por favor, ingrese un valor válido para este campo.
                     </div>
                 </div>
                 <div class="mb-3">
                     <label for="validationCustom01" class="form-label">Horario de cierre:</label>
-                    <input type="text" name="hora-cierre" class="form-control" id="validationCustom01" pattern="^(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$" autocomplete="off" spellcheck="false" 
+                    <input type="text" name="hora-cierre" class="form-control" id="validationCustom01" pattern="^(0?[0-9]|1[0-9]|2[0-2]):[0-5][0-9]:[0-5][0-9]$" autocomplete="off" spellcheck="false" 
                     minlength="8" maxlength="8" placeholder="HH:MM:SS" required>
-                    <div class="invalid-feedback">
-                        Por favor, ingrese un valor válido para este campo.
-                    </div>
+                   
+                    <div class="invalid-feedback" id="ci">Por favor, ingrese un valor válido para este campo.</div>
                 </div>
+                
                 <div class="mb-3">
                     <label for="validationCustom02" class="form-label">Día:</label>
                     <select class="form-select" name="dia" id="validationCustom04" required>
@@ -55,7 +56,7 @@ include('../models/funcionConfiguracionHorario.php')
                     <option class='form-control' id='validationCustom02' value="DOMINGO">Domingo</option>
                     </select>
                     <div class="invalid-feedback">
-                        Por favor seleccione un sitio
+                        Por favor seleccione un dia
                     </div>
                 </div>
                 
@@ -80,6 +81,7 @@ include('../models/funcionConfiguracionHorario.php')
                         </div>
                     </div>
                 </div>
+
             </form>
         </div>
     </div>
@@ -95,7 +97,46 @@ include('../models/funcionConfiguracionHorario.php')
 
     ?>
     
-    
+    <script>
+   function validarHorario() {
+  // Obtener los valores de ambos campos de entrada y convertirlos en objetos de fecha
+  const horaApertura = document.getElementsByName("hora-apertura")[0];
+  const horaCierre = document.getElementsByName("hora-cierre")[0];
+  
+  const apertura = new Date("2000-01-01T" + horaApertura.value + "Z");
+  const cierre = new Date("2000-01-01T" + horaCierre.value + "Z");
+
+  // Validar que el horario de apertura sea anterior al de cierre
+  if (apertura >= cierre) {
+    var miv="Error la hora de apertura debe ser mayor a la hora de cierre";
+      horaCierre.setCustomValidity("errorr");
+      document.getElementById("ci").innerHTML = miv;
+      
+  } else {
+    if (cierre.getTime() - apertura.getTime() < 3 * 60 * 60 * 1000) {
+        var miv="Error el tiempo de funcionamiento del parqueo es de 3 horas.";
+       horaCierre.setCustomValidity("errorr");
+       document.getElementById("ci").innerHTML = miv;
+    }else{
+    horaCierre.setCustomValidity("");}
+  }
+}
+</script>
+<script>
+  const myForm = document.getElementById("myForm");
+  myForm.addEventListener("submit", function(event) {
+   event.preventDefault();
+    validarHorario();
+  });
+  
+</script>
+
+
+
+
+
+
+   
 <script src="/SistemaParqueo/public/js/validacion.js"></script>
 </body>
 </html>
