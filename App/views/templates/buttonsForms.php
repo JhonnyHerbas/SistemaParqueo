361ca071f5f723823b2804a3a12f784d9a8b941e
JhonnyHerbas <65179031+JhonnyHerbas@users.noreply.github.com>
2023-04-25 00:23:42 -0400

    Solucion errores

--- App/views/templates/buttonsForms.php
+++
@@ -1,4 +0,0 @@
-<div class="col-12 button">
-    <button class="btn btn-success" id="submitButton" data-toggle="modal" data-target="#exampleModal"><?php echo $success; ?></button>
-    <button class="btn btn-danger" type="reset"><?php echo $danger; ?></button>
-</div>
