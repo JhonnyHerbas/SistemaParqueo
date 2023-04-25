47d3af67fc85289dc83521de7006892698a5cfd4
JhonnyHerbas <65179031+JhonnyHerbas@users.noreply.github.com>
2023-04-25 00:16:55 -0400

    Delete eliminarSeccionAction.php

--- App/controllers/eliminarSeccionAction.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include("../models/funcionSeccion.php");
-/*Se debe obtener el id_sit y el id_sec*/
-eliminar_seccion($_POST['id_sec']);
-header("Location: /SistemaParqueo/App/views/visualizarSitio.php");
-exit();
-?>
