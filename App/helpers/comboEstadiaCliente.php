eea78634d742f5c058b2e293564616612b1d4865
jhamil06 <128666918+jhamil06@users.noreply.github.com>
2023-05-07 22:25:18 -0400

    seborrasolitosad

--- App/helpers/comboEstadiaCliente.php
+++ App/helpers/comboEstadiaCliente.php
@@ -1,34 +1,26 @@
 <?php
 require_once('../config/conexion.php');
 
-function visualizar_reporte($anio){
+function visualizar_reporte($fecha){
 	$conn = get_connection();
-	$query = 'CALL DB_SP_VISTA_REPORTES_SEMANAL("'.$anio.'")';
+	$query = 'CALL DB_SP_VISTA_ESTADIA("'.$fecha.'")';
 	$result = $conn->query($query);
 	$json = array();
-	$mesAnterior=null;
-	while($row= $result->fetch_array(MYSQLI_BOTH)){
-		//$semana =$row['SEMANA'];
-		//$mes = substr($semana,0,strpos($semana, ' '));
-	
-		
+
+	while($row = $result->fetch_array(MYSQLI_BOTH)){
 		$json[] = array(
-			//rellenamos dependiendo al procedimineto alamacenado
-			'SEMANA_ANIO' => $row['SEMANA_ANIO'],
-			'SEMANA' => ucfirst(strtolower($mes)).' Semana '.$j , 
-            'NUM_SEMANA' => $row['NUM_SEMANA']+1,
-			'NUM_SEMANA_PDF' => $row['NUM_SEMANA'],
-			'ANIO' => $row['ANIO'],
-			'TOTAL_FACTURA' => $row['TOTAL_FACTURA'],
-			'TOTAL_SITIOS' => $row['TOTAL_SITIOS'],
-			'NUEVOS_SITIOS' => $row['NUEVOS_SITIOS'],
-           
+			'NOMBRE_DOC' => $row['NOMBRE_DOC'],
+			'APELLIDO_DOC' => $row['APELLIDO_DOC'],
+			'FECHA_INGRESO_ACT' => $row['FECHA_INGRESO_ACT'],
+			'FECHA_SALIDA_ACT' => $row['FECHA_SALIDA_ACT'],
+			'TIEMPO_TRANSCURRIDO' => $row['TIEMPO_TRANSCURRIDO']
 		);
-		
 	}
+	
 	$jsonString = json_encode($json);
 	echo $jsonString;
 }
-$anio = $_POST['ANIO'];
-visualizar_reporte($anio);
+
+$fecha = $_POST['fecha'];
+visualizar_reporte($fecha);
 ?>
--- public/js/comboEstadiaCliente.js
+++ public/js/comboEstadiaCliente.js
@@ -2,60 +2,58 @@ $(document).ready(function () {
     let select = document.getElementById("fecha");
     let valorSeleccionado = select.value;
     let data = document.getElementById("data");
+
     function cargar_reportes(send_dato) {
-       
+        console.log("Contenido de select:", select);
+        console.log("Contenido de send_dato:", send_dato);
         $.ajax({
             type: "POST",
             url: "/SistemaParqueo/App/helpers/comboEstadiaCliente.php",
             data: send_dato,
             success: function (response) {
-                const reportes = JSON.parse(response)
+                const reportes = JSON.parse(response);
                 let templete = ``;
                 let i = 1;
-                let j= 1;
-                console.log(reportes)
-                reportes.forEach(reportes => {
+                let j = 1;
+                console.log(reportes);
+                reportes.forEach(reporte => {
                     templete += `
                     <div class="accordion-item">
                         <h2 class="accordion-header">
                             <button class="accordion-button collapsed titulo-acordion" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#flush-collapse${i}" aria-expanded="false" aria-controls="flush-collapseTwo">
-                             
-                                ${reportes.NOMBRE_CLI} 
+                                data-bs-target="#flush-collapse${i}" aria-expanded="false" aria-controls="flush-collapse${i}">
+                                
+                                ${reporte.NOMBRE_DOC} 
                             </button>
                         </h2>
                         <div id="flush-collapse${i}" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
                             <div class="accordion-body body-sitio">
                                 <div class="acordion-text w-50">
-                                    Entrada: ${reportes.FECHA_ENTRADA} <br> 
-                                    Salida: ${reportes.FECHA_SALIDA}<br> 
-                                    
+                                    Entrada: ${reporte.FECHA_INGRESO_ACT} <br> 
+                                    Salida: ${reporte.FECHA_SALIDA_ACT}<br> 
+                                    Tiempo Transcurrido: ${reporte.TIEMPO_TRANSCURRIDO}<br>
                                 </div>
                             </div>
                         </div>
                     </div>            
                     `;
-                    
                     i++;
-                    
                 });
                 data.innerHTML = templete;
             }
         });
-        semanaAnterior = reportes.semana;
     }
-    let semanaAnterior = null;
+   
     let send_dato = {
-        'ANIO': valorSeleccionado
+        'fecha': select.value
     };
 
     cargar_reportes(send_dato);
 
-    select.addEventListener('change', function () {
+    document.getElementById("buscar").addEventListener('click', function () {
         send_dato = {
-            'ANIO': select.value
+            'fecha': select.value
         };
         cargar_reportes(send_dato);
     });
-
-})
+});
