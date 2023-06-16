338a097067f170b9736203e353fb702e36e70a97
darwing-21 <darwingarcia2130@gmail.com>
2023-06-16 01:02:16 -0400

    aceptacion y rechazo de sitios compartidos

---
+++ App/controllers/rechazarSitioCompartido.php
@@ -0,0 +1,59 @@
+<?php 
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\PHPMailer;
+//require_once 'vendor/autoload.php';
+require '../../vendor/phpMailer/phpmailer/src/Exception.php';
+require '../../vendor/phpMailer/phpmailer/src/PHPMailer.php';
+require '../../vendor/phpmailer/phpmailer/src/SMTP.php';
+
+include('../models/funcionAdmin.php');
+include('../models/funcionDocente.php');
+
+// Crea un objeto PHPMailer
+$mail= new PHPMailer(true);
+ // Configura el servidor SMTP de Gmail
+ $mail->isSMTP();
+ $mail->Host = 'smtp.gmail.com';
+ $mail->Port = 465;
+ $mail->SMTPAuth = true;
+ $mail->SMTPSecure = 'ssl';
+
+ // Configura las credenciales de Gmail
+ $mail->Username = 'servicio.correo.exodus@gmail.com';
+ $mail->Password = 'frmstpfizdzgjqei';
+
+ $codigo = $_GET["codigo"];
+ $result = visualizar_sitio_compartido_id($codigo);
+ $compartido= $result->fetch_array(MYSQLI_BOTH);
+
+ $docentes = visualizar_docente_id($compartido['ID_TITULAR_COMP']);
+ $docente= $docentes->fetch_array(MYSQLI_BOTH);
+
+ $correo = $docente["CORREO_DOC"];
+
+ // Configura el remitente y el destinatario del correo
+ $mail->setFrom('servicio.correo.exodus@gmail.com', 'Sistema de parqueo FCYT');
+ $mail->addAddress($correo, $codigo);
+
+ try {       
+    // Configura el asunto y el cuerpo del correo
+    $mail->Subject = 'Rechazo de solicitud de sitio compartido';
+    $mail->Body    = 'Estimado/a Docente,
+    Esperamos que este mensaje le encuentre bien. Nos dirigimos a usted para informarle que su solicitud realizada a través del sitio compartido, lamentablemente ha sido rechazada en el Sistema de Parqueo FCYT.
+    
+    Lamentamos cualquier inconveniente que esto pueda ocasionarle y comprendemos que pueda generar cierta frustración. Sin embargo, debido a ciertas circunstancias o criterios específicos, no hemos podido otorgar la aprobación solicitada en esta ocasión.
+           
+    Agradecemos su comprensión y le aseguramos que estamos trabajando constantemente para mejorar nuestros servicios y procesos. Si tiene alguna pregunta adicional o requiere más información, no dude en ponerse en contacto con nuestro equipo de atención al cliente, quienes estarán encantados de ayudarle.
+    
+    Atentamente,    
+    Sistema de Parqueo FCYT';
+
+    // Envía el correo electrónico y muestra un mensaje
+    $mail->send();
+    rechazar_sitio_compartido($codigo);
+    header("Location: ../views/visualizarSolicitudCompartido.php");
+} catch (Exception $e) {
+    echo 'Error al enviar el correo electrónico: ' . $mail->ErrorInfo;
+}
+?>
--- App/models/funcionAdmin.php
+++ App/models/funcionAdmin.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once('../config/conexion.php') ;
+require_once('../config/conexion.php');
 
-function iniciar_sesion($user){
+function iniciar_sesion($user)
+{
     $conn = get_connection();
     $stmt = mysqli_prepare($conn, "CALL DB_SP_ADMINISTRADOR_CONTRASENA(?)");
     mysqli_stmt_bind_param($stmt, "s", $user);
@@ -15,10 +16,11 @@ function iniciar_sesion($user){
         return null;
     }
 }
-function insertar_docente ($codigo, $nombre, $apellido, $celular, $correo, $pass) {
+function insertar_docente($codigo, $nombre, $apellido, $celular, $correo, $pass)
+{
     $conn = get_connection();
     $stmt = $conn->prepare("CALL DB_SP_DOCENTE_INSERTAR(?,?,?,?,?,?)");
-    $stmt->bind_param("ississ",$codigo, $nombre, $apellido, $celular, $correo, $pass);
+    $stmt->bind_param("ississ", $codigo, $nombre, $apellido, $celular, $correo, $pass);
     if ($stmt->execute()) {
         $stmt->close();
         return true;
@@ -28,7 +30,8 @@ function insertar_docente ($codigo, $nombre, $apellido, $celular, $correo, $pass
     }
 }
 
-function visualizar_docente(){
+function visualizar_docente()
+{
     $conn = get_connection();
     $query = 'SELECT * FROM DB_VIEW_DOCENTE_VISTA';
 
@@ -39,19 +42,21 @@ function visualizar_docente(){
     }
 }
 
-function visualizar_datos_docente($ID_DOC) {
+function visualizar_datos_docente($ID_DOC)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_DOCENTE_VISTA_EDITAR("'.$ID_DOC.'")';
+    $query = 'CALL DB_SP_DOCENTE_VISTA_EDITAR("' . $ID_DOC . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
         return null;
     }
 }
-function editar_docente_contrasena($ID_DOC, $CONTRASENA_DOC) {
+function editar_docente_contrasena($ID_DOC, $CONTRASENA_DOC)
+{
     $conn = get_connection();
     $stmt = $conn->prepare("CALL DB_SP_DOCENTE_CAMBIO_CONTRASENA(?,?)");
-    $stmt->bind_param("is",$ID_DOC, $CONTRASENA_DOC);
+    $stmt->bind_param("is", $ID_DOC, $CONTRASENA_DOC);
     if ($stmt->execute()) {
         $stmt->close();
         return true;
@@ -61,10 +66,11 @@ function editar_docente_contrasena($ID_DOC, $CONTRASENA_DOC) {
     }
 }
 
-function editar_docente($ID_DOC, $ID_DOC_NUE,$NOMBRE_DOC, $APELLIDO_DOC, $CELULAR_DOC, $CORREO_DOC) {
+function editar_docente($ID_DOC, $ID_DOC_NUE, $NOMBRE_DOC, $APELLIDO_DOC, $CELULAR_DOC, $CORREO_DOC)
+{
     $conn = get_connection();
     $stmt = $conn->prepare("CALL DB_SP_DOCENTE_ACTUALIZAR(?,?,?,?,?,?)");
-    $stmt->bind_param("iissis",$ID_DOC, $ID_DOC_NUE,$NOMBRE_DOC, $APELLIDO_DOC, $CELULAR_DOC, $CORREO_DOC);
+    $stmt->bind_param("iissis", $ID_DOC, $ID_DOC_NUE, $NOMBRE_DOC, $APELLIDO_DOC, $CELULAR_DOC, $CORREO_DOC);
     if ($stmt->execute()) {
         $stmt->close();
         return true;
@@ -73,9 +79,10 @@ function editar_docente($ID_DOC, $ID_DOC_NUE,$NOMBRE_DOC, $APELLIDO_DOC, $CELULA
         return false;
     }
 }
-function ver_contrasena($ID_DOC){
+function ver_contrasena($ID_DOC)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_DOCENTE_CONTRASENA("'.$ID_DOC.'")';
+    $query = 'CALL DB_SP_DOCENTE_CONTRASENA("' . $ID_DOC . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
@@ -83,18 +90,20 @@ function ver_contrasena($ID_DOC){
     }
 }
 
-function obtener_reporte_mensual_pdf($mes,$anio){
+function obtener_reporte_mensual_pdf($mes, $anio)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_REPORTE_MENSUAL_PDF("'.$mes.'","'.$anio.'")';
+    $query = 'CALL DB_SP_REPORTE_MENSUAL_PDF("' . $mes . '","' . $anio . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
         return null;
     }
 }
-function obtener_reporte_semanal_pdf($semana,$anio){
+function obtener_reporte_semanal_pdf($semana, $anio)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_REPORTE_SEMANAL_PDF("'.$semana.'","'.$anio.'")';
+    $query = 'CALL DB_SP_REPORTE_SEMANAL_PDF("' . $semana . '","' . $anio . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
@@ -102,7 +111,8 @@ function obtener_reporte_semanal_pdf($semana,$anio){
     }
 }
 
-function visualizar_compra_moneda(){
+function visualizar_compra_moneda()
+{
     $conn = get_connection();
     $query = 'SELECT * FROM DB_VIEW_COMPRA_VISTA';
 
@@ -113,9 +123,10 @@ function visualizar_compra_moneda(){
     }
 }
 
-function visualizar_asignacion_moneda($ID_COM){
+function visualizar_asignacion_moneda($ID_COM)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_COMPRA_VISTA("'.$ID_COM.'")';
+    $query = 'CALL DB_SP_COMPRA_VISTA("' . $ID_COM . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
@@ -123,10 +134,11 @@ function visualizar_asignacion_moneda($ID_COM){
     }
 }
 
-function asignar_moneda($ID_DOC,$CANT,$ID_COM){
+function asignar_moneda($ID_DOC, $CANT, $ID_COM)
+{
     $conn = get_connection();
     $stmt = $conn->prepare("CALL DB_SP_COMPRA_ASIGNAR_MONEDA(?,?,?)");
-    $stmt->bind_param("iii",$ID_DOC,$CANT,$ID_COM);
+    $stmt->bind_param("iii", $ID_DOC, $CANT, $ID_COM);
     if ($stmt->execute()) {
         $stmt->close();
         return true;
@@ -136,10 +148,11 @@ function asignar_moneda($ID_DOC,$CANT,$ID_COM){
     }
 }
 
-function rechazar_moneda($ID_COM){
+function rechazar_moneda($ID_COM)
+{
     $conn = get_connection();
     $stmt = $conn->prepare("CALL DB_SP_COMPRA_RECHAZAR_MONEDA(?)");
-    $stmt->bind_param("i",$ID_COM);
+    $stmt->bind_param("i", $ID_COM);
     if ($stmt->execute()) {
         $stmt->close();
         return true;
@@ -149,10 +162,11 @@ function rechazar_moneda($ID_COM){
     }
 }
 
-function insertar_guardia($ci,$cod, $nombre, $apellido, $pass) {
+function insertar_guardia($ci, $cod, $nombre, $apellido, $pass)
+{
     $conn = get_connection();
     $stmt = $conn->prepare("CALL DB_SP_GUARDIA_INSERTAR(?,?,?,?,?)");
-    $stmt->bind_param("iisss",$ci,$cod,$nombre, $apellido, $pass);
+    $stmt->bind_param("iisss", $ci, $cod, $nombre, $apellido, $pass);
     if ($stmt->execute()) {
         $stmt->close();
         return true;
@@ -162,7 +176,8 @@ function insertar_guardia($ci,$cod, $nombre, $apellido, $pass) {
     }
 }
 
-function visualizar_guardia(){
+function visualizar_guardia()
+{
     $conn = get_connection();
     $query = 'SELECT * FROM DB_VIEW_GUARDIA_VISTA';
 
@@ -173,7 +188,8 @@ function visualizar_guardia(){
     }
 }
 
-function visualizar_guardia_activo(){
+function visualizar_guardia_activo()
+{
     $conn = get_connection();
     $query = 'SELECT * FROM DB_VIEW_GUARDIA_ACTIVO_VISTA';
 
@@ -184,9 +200,10 @@ function visualizar_guardia_activo(){
     }
 }
 
-function visualizar_guardia_id($id){
+function visualizar_guardia_id($id)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_GUARDIA_VISTA_ID("'.$id.'")';
+    $query = 'CALL DB_SP_GUARDIA_VISTA_ID("' . $id . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
@@ -194,9 +211,10 @@ function visualizar_guardia_id($id){
     }
 }
 
-function habilitar_guardia($id){
+function habilitar_guardia($id)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_GUARDIA_HABLITAR("'.$id.'")';
+    $query = 'CALL DB_SP_GUARDIA_HABLITAR("' . $id . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
@@ -204,9 +222,10 @@ function habilitar_guardia($id){
     }
 }
 
-function deshabilitar_guardia($id){
+function deshabilitar_guardia($id)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_GUARDIA_DESHABLITAR("'.$id.'")';
+    $query = 'CALL DB_SP_GUARDIA_DESHABLITAR("' . $id . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
@@ -214,7 +233,8 @@ function deshabilitar_guardia($id){
     }
 }
 
-function visualizar_horario(){
+function visualizar_horario()
+{
     $conn = get_connection();
     $query = 'SELECT * FROM DB_VIEW_HORARIO_VISTA';
 
@@ -225,9 +245,10 @@ function visualizar_horario(){
     }
 }
 
-function asignar_guardia_horario($id_horario,$id_guardia){
+function asignar_guardia_horario($id_horario, $id_guardia)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_HORARIO_GUARDIA_ASIGNAR("'.$id_horario.'","'.$id_guardia.'")';
+    $query = 'CALL DB_SP_HORARIO_GUARDIA_ASIGNAR("' . $id_horario . '","' . $id_guardia . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
@@ -235,10 +256,11 @@ function asignar_guardia_horario($id_horario,$id_guardia){
     }
 }
 
-function insertar_horario($ingreso,$salida, $turno, $sueldo) {
+function insertar_horario($ingreso, $salida, $turno, $sueldo)
+{
     $conn = get_connection();
     $stmt = $conn->prepare("CALL DB_SP_HORARIO_INSERTAR(?,?,?,?)");
-    $stmt->bind_param("sssi",$ingreso,$salida,$turno, $sueldo);
+    $stmt->bind_param("sssi", $ingreso, $salida, $turno, $sueldo);
     if ($stmt->execute()) {
         $stmt->close();
         return true;
@@ -248,9 +270,10 @@ function insertar_horario($ingreso,$salida, $turno, $sueldo) {
     }
 }
 
-function visualizar_horario_id($id){
+function visualizar_horario_id($id)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_HORARIO_VISTA_ID("'.$id.'")';
+    $query = 'CALL DB_SP_HORARIO_VISTA_ID("' . $id . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
@@ -258,10 +281,11 @@ function visualizar_horario_id($id){
     }
 }
 
-function editar_horario($id,$ingreso, $salida, $turno, $sueldo) {
+function editar_horario($id, $ingreso, $salida, $turno, $sueldo)
+{
     $conn = get_connection();
     $stmt = $conn->prepare("CALL DB_SP_HORARIO_UPDATE(?,?,?,?,?)");
-    $stmt->bind_param("isssi",$id,$ingreso,$salida,$turno,$sueldo);
+    $stmt->bind_param("isssi", $id, $ingreso, $salida, $turno, $sueldo);
     if ($stmt->execute()) {
         $stmt->close();
         return true;
@@ -271,9 +295,10 @@ function editar_horario($id,$ingreso, $salida, $turno, $sueldo) {
     }
 }
 
-function eliminar_horario($id){
+function eliminar_horario($id)
+{
     $conn = get_connection();
-    $query ='CALL DB_SP_HORARIO_ELIMINAR("'.$id.'")';
+    $query = 'CALL DB_SP_HORARIO_ELIMINAR("' . $id . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
@@ -281,10 +306,11 @@ function eliminar_horario($id){
     }
 }
 
-function editar_guardia($id,$nombre, $apellido) {
+function editar_guardia($id, $nombre, $apellido)
+{
     $conn = get_connection();
     $stmt = $conn->prepare("CALL DB_SP_GUARDIA_UPDATE(?,?,?)");
-    $stmt->bind_param("iss",$id,$nombre,$apellido);
+    $stmt->bind_param("iss", $id, $nombre, $apellido);
     if ($stmt->execute()) {
         $stmt->close();
         return true;
@@ -294,10 +320,11 @@ function editar_guardia($id,$nombre, $apellido) {
     }
 }
 
-function insertar_sitio_compartido($id,$titular, $suplente) {
+function insertar_sitio_compartido($id, $titular, $suplente)
+{
     $conn = get_connection();
     $stmt = $conn->prepare("CALL DB_SP_COMPARTIDO_INSERT(?,?,?)");
-    $stmt->bind_param("iii",$id,$titular,$suplente);
+    $stmt->bind_param("iii", $id, $titular, $suplente);
     if ($stmt->execute()) {
         $stmt->close();
         return true;
@@ -307,7 +334,8 @@ function insertar_sitio_compartido($id,$titular, $suplente) {
     }
 }
 
-function visualizar_sitio_compartido(){
+function visualizar_sitio_compartido()
+{
     $conn = get_connection();
     $query = 'SELECT * FROM DB_VIEW_COMPARTIDO_VISTA';
 
@@ -318,45 +346,10 @@ function visualizar_sitio_compartido(){
     }
 }
 
-function crear_noticia($id, $titulo, $descripcion){
+function visualizar_sitio_compartido_id($id)
+{
     $conn = get_connection();
-    $stmt = $conn->prepare("CALL DB_SP_NOTICIA_CREAR(?,?,?)");
-    $stmt->bind_param("iss",$id, $titulo, $descripcion);
-    if ($stmt->execute()) {
-        $stmt->close();
-        return true;
-    } else {
-        $stmt->close();
-        return false;
-    }
-}
-function editar_noticia($id, $titulo, $descripcion){
-    $conn = get_connection();
-    $stmt = $conn->prepare("CALL DB_SP_NOTICIA_EDITAR(?,?,?)");
-    $stmt->bind_param("iss",$id, $titulo, $descripcion);
-    if ($stmt->execute()) {
-        $stmt->close();
-        return true;
-    } else {
-        $stmt->close();
-        return false;
-    }
-}
-function eliminar_noticia($id){
-    $conn = get_connection();
-    $stmt = $conn->prepare("CALL DB_SP_NOTICIA_ELIMINAR(?)");
-    $stmt->bind_param("i",$id);
-    if ($stmt->execute()) {
-        $stmt->close();
-        return true;
-    } else {
-        $stmt->close();
-        return false;
-    }
-}
-function ver_noticia($id){
-    $conn = get_connection();
-    $query ='CALL DB_SP_NOTICIA_VER("'.$id.'")';
+    $query = 'CALL DB_SP_COMPARTIDO_VISTA_ID("' . $id . '")';
     if ($result = $conn->query($query)) {
         return $result;
     } else {
@@ -364,7 +357,58 @@ function ver_noticia($id){
     }
 }
 
-function visualizar_noticias(){
+function crear_noticia($id, $titulo, $descripcion)
+{
+    $conn = get_connection();
+    $stmt = $conn->prepare("CALL DB_SP_NOTICIA_CREAR(?,?,?)");
+    $stmt->bind_param("iss", $id, $titulo, $descripcion);
+    if ($stmt->execute()) {
+        $stmt->close();
+        return true;
+    } else {
+        $stmt->close();
+        return false;
+    }
+}
+function editar_noticia($id, $titulo, $descripcion)
+{
+    $conn = get_connection();
+    $stmt = $conn->prepare("CALL DB_SP_NOTICIA_EDITAR(?,?,?)");
+    $stmt->bind_param("iss", $id, $titulo, $descripcion);
+    if ($stmt->execute()) {
+        $stmt->close();
+        return true;
+    } else {
+        $stmt->close();
+        return false;
+    }
+}
+function eliminar_noticia($id)
+{
+    $conn = get_connection();
+    $stmt = $conn->prepare("CALL DB_SP_NOTICIA_ELIMINAR(?)");
+    $stmt->bind_param("i", $id);
+    if ($stmt->execute()) {
+        $stmt->close();
+        return true;
+    } else {
+        $stmt->close();
+        return false;
+    }
+}
+function ver_noticia($id)
+{
+    $conn = get_connection();
+    $query = 'CALL DB_SP_NOTICIA_VER("' . $id . '")';
+    if ($result = $conn->query($query)) {
+        return $result;
+    } else {
+        return null;
+    }
+}
+
+function visualizar_noticias()
+{
     $conn = get_connection();
     $query = 'SELECT * FROM DB_VIEW_NOTICIAS_VISTA';
 
@@ -374,4 +418,24 @@ function visualizar_noticias(){
         return null;
     }
 }
-?>
+
+function rechazar_sitio_compartido($ID)
+{
+    $conn = get_connection();
+    $query = 'CALL DB_SP_COMPARTIDO_RECHAZO("' . $ID . '")';
+    if ($result = $conn->query($query)) {
+        return $result;
+    } else {
+        return null;
+    }
+}
+function aceptar_sitio_compartido($ID)
+{
+    $conn = get_connection();
+    $query = 'CALL DB_SP_COMPARTIDO_ACEPTAR("' . $ID . '")';
+    if ($result = $conn->query($query)) {
+        return $result;
+    } else {
+        return null;
+    }
+}
