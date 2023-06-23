47deab9c385b5f84dccdc54136088bf3d88a96ac
darwing-21 <darwingarcia2130@gmail.com>
2023-06-23 17:31:40 -0400

    con ajax visualizar consultas

---
+++ App/helpers/comboConsulta.php
@@ -0,0 +1,29 @@
+<?php 
+require_once('../config/conexion.php');
+
+function visualizar_consulta($estado)
+{
+    $conn = get_connection();
+    $query = 'CALL DB_SP_CONSULTA_VISTA_ESTADO("'.$estado.'")';
+    $result = $conn->query($query);
+    $json = array();
+    while($row= $result->fetch_array(MYSQLI_BOTH)){
+        $json[] = array(
+            'ID_REC' => $row['ID_REC'],
+            'TITULO_REC' => $row['TITULO_REC'],
+            'DESCRIPCION_REC' => $row['DESCRIPCION_REC'],
+            'FECHA_REC' => $row['FECHA_REC'],
+            'ESTADO_REC' => $row['ESTADO_REC'],
+            'ID_DOC' => $row['ID_DOC'],
+            'NOMBRE_DOC' => $row['NOMBRE_DOC'],
+            'APELLIDO_DOC' => $row['APELLIDO_DOC'],
+            'CELULAR_DOC' => $row['CELULAR_DOC'],
+            'CORREO_DOC' => $row['CORREO_DOC']
+        );
+    }
+    $jsonString = json_encode($json);
+    echo $jsonString;
+}
+$estado = $_POST['ESTADO'];
+visualizar_consulta($estado);
+?>
--- App/views/visualizarConsulta.php
+++ App/views/visualizarConsulta.php
@@ -15,71 +15,34 @@ include('templates/head.php');
     if ($_SESSION['rol'] != "Administrador") {
         header('Location: visualizarSitio.php');
     }
-    include('../models/funcionAdmin.php');
-    include('../models/funcionDocente.php');
-    include('../models/funcionReclamo.php');
     ?>
 
     <!-- Aqui vendra toda la interfaz que se necesita para la visualizacion -->
     <div class="container container-solicitud ">
         <div class="solicitud-header">
             <h3 class="font-weight-bold">
-                CONSULTAS
+                Consultas
             </h3>
+            <select class="form-select text" aria-label="Default select example" id="estado">
+                <option selected disabled>ESTADO</option>
+                <option value="SOLUCIONADO">SOLUCIONADO</option>
+                <option value="RECHAZADO">RECHAZADO</option>
+                <option value="ESPERA" selected>ESPERA</option>
+            </select>
         </div>
-        <div class="data">
-            <?php 
-             $result = visualizar_consulta();
-             $i=1;
-             if($result){
-                while ($row= $result->fetch_array(MYSQLI_BOTH)){
-                    $collapse = "flush-collapse".$i;                                
-                    ?>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed titulo-acordion" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo $collapse ?>" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                <?php 
-                                    echo 'Consulta '. $i;                                        
-                                ?>
-                            </button>
-                        </h2>
-                        <div id="<?php echo $collapse ?>" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body body-sitio">
-                                <div class="acordion-text w-80">
-                                <?php
-                                    $docentes = visualizar_docente_id($row['ID_DOC']);
-                                    $docente= $docentes->fetch_array(MYSQLI_BOTH);
-                                    echo "Título: ".$row["TITULO_REC"]. '<br>';
-                                    echo "Nombre: ".$docente["NOMBRE_DOC"].' '.$docente["APELLIDO_DOC"]. '<br>';
-                                    echo "Celular: ".$docente["CELULAR_DOC"]. '<br>';
-                                    echo "Correo: ".$docente["CORREO_DOC"]. '<br>';
-                                    echo "Descripción: ".$row["DESCRIPCION_REC"]. '<br>';
-                                    echo "Fecha: ".$row["FECHA_REC"]. '<br>';
-                                    
-                                ?>
-                                </div>
-                                <div class="acordion-btn w-50">
-                                    <div class="function verde">
-                                    <a href="responderConsulta.php?consulta=<?php echo $row["ID_REC"]; ?>" class="fa-solid fa-clipboard-question blanco"></a>
-                                    </div>
-                                </div>                                    
-                            </div>
-                        </div>
-                    </div>                      
-            <?php
-                $i=$i+1;                        
-                }
-            }
-            ?>
+
+        <div class="data" id="data">
+
         </div>
     </div>
-        
-        <!-- Include de los scripts.php -->
-        <?php
 
-        include('templates/scripts.php');
+    <!-- Include de los scripts.php -->
+    <?php
 
-        ?>
+    include('templates/scripts.php');
+
+    ?>
+    <script src="../../public/js/comboEstadoConsulta.js"></script>
 </body>
 
 </html>
